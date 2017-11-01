1d958461b5457260deea30add487d91a543b9c79
eren.rudy <eren@rudy-web.ca>
2017-10-31 22:47:24 -0400

    Added script for optimization of compressive member

---
+++ Compressive_Beam_Optimization.m
@@ -0,0 +1,19 @@
+buckling_load = 3.59245*(10^2); %Newtons
+member_length = 0.3; %Metres
+t = 0.0032;
+youngs_mod_member = 2.37*(10^9); %Pa
+I_min = buckling_load*(member_length^2)/((pi^2)*youngs_mod_member)
+
+%function y = compute_x()
+
+syms x;
+eqn = I_min == 1/12*((t*2 + x)^4 - x^4);
+solx = solve(eqn, x, 'Real', true);
+vpa(solx)
+
+
+%end
+
+
+
+
